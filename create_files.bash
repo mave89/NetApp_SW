7b9f249e4c7de21080fea32edac7cce5a07ca946
faizabidi <fabidi89@vt.edu>
2019-06-30 15:22:55 -0700

    Found a faster way to create tons of files.

--- create_files.bash
+++ create_files.bash
@@ -5,12 +5,18 @@ NUM=$2
 
 if [ $# -ne 2 ]; then
     echo "Please enter the size of the file (KB) and the number of files to be created."
-    echo "For example - ./create_files.bash 1024 10"
+    echo "For example - ./create_files.bash 4 10"
     exit 1
 fi
 
-# Create files
-for ((i=0; i<$NUM; i++)); do
-    dd if=/dev/zero of=file$i.txt count=1024 bs=$SIZE
-done
+# Create a data directory and put all files in it
+mkdir -p data && cd data
+
+# Create 1 big master file
+TOTAl_SIZE=$(($SIZE*1024*$NUM))
+dd if=/dev/zero of=masterfile bs=1 count=$TOTAl_SIZE
+
+# Split it up
+SIZE_OF_FILES=$(($SIZE * 1024))
+split -b $SIZE_OF_FILES -a 10 masterfile
 
